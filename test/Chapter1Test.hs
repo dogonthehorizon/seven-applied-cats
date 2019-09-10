62f3f4f90faaeb5e107282ab76edf04f88f3c8a0
Fernando Freire <dogonthehorizon@gmail.com>
2019-09-09 21:21:03 -0700

    Add exercise 1.1

---
+++ test/Chapter1Test.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Chapter1Test where
+
+import Test.SmallCheck.Series (Serial)
+import Test.Tasty             (TestTree, testGroup)
+import Test.Tasty.SmallCheck  (testProperty, (==>))
+
+-- | Exercise 1.1 (pg. 2)
+--
+-- Some terminology: a function f: R -> R is said to be
+-- a) order-preserving if x <= y implies f(x) <= f(y), for all x, y in R
+-- b) metric-preserving if |x-y| = |f(x) - f(y)|
+-- c) addition-preserving if f(x+y) = f(x) + f(y)
+--
+-- For each of the three properties defined above --call it foo --find an f that
+-- is foo-preserving and an example of an f that is not foo-preserving
+
+fooPreservingSuite
+  :: (Num x, Ord x, Show x, Serial IO x) => String -> (x -> x) -> TestTree
+fooPreservingSuite fnName fn = testGroup fnName
+  [ testProperty "order-preserving" $ \(x, y) -> x < y ==> fn x < fn y
+  , testProperty "metric-perserving"
+    $ \(x, y) -> abs (x - y) == abs (fn x - fn y)
+  , testProperty "addition-preserving" $ \(x, y) -> fn (x + y) == fn x + fn y
+  ]
+
+-- | Exercise 1.1 Tests
+test_idIsFooPreserving = fooPreservingSuite "id" (id :: Int -> Int)
+
+-- | Exercise 1.1 Tests that are expected to fail
+-- test_halfIsNotFooPreserving   = fooPreservingSuite "half" (\ (x :: Int) -> div x 2)
+-- test_doubleIsNotFooPreserving = fooPreservingSuite "double" (\ (x :: Int) -> x * 2)
+-- test_powTwoIsNotFooPreserving = fooPreservingSuite "powTwo" (\ (x :: Int) -> x ^ 2)
