ba4ad20b528f4b253e9509037d808e3c8558111c
Fernando Freire <dogonthehorizon@gmail.com>
2019-09-19 06:25:24 -0700

    Add fn to group partitions by set size

--- src/Chapter1.hs
+++ src/Chapter1.hs
@@ -1,5 +1,6 @@
 module Chapter1 where
 
+import Data.Function (on)
 import qualified Data.List as List
 
 -- Exercise 1.3
@@ -14,16 +15,22 @@ hasseComparator xs ys =
 partitions :: Ord a => [a] -> [[a]]
 partitions = List.sortBy hasseComparator . tail . List.subsequences
 
+-- | Determine the possible combinations of r sub-elements of a set with n elements
 possibleCombinations :: Int -> Int -> Int
 possibleCombinations n r =
   let fact x = product [1..x]
   in div (fact n) ((fact r) * (fact $ n - r))
 
+-- | Determine the possible partitions of a mathematical set.
 possiblePartitions :: [a] -> Int
 possiblePartitions xs =
   let n = length xs
   in
     sum $ (possibleCombinations n) <$> [1..n]
+
+-- | Given a list of partitions, group each partition by it's length
+groupPartitions :: [[a]] -> [[[a]]]
+groupPartitions = List.groupBy ((==) `on` length)
 
 -- | TODO see how much effort it would be to create a directed graph from the
 -- output of partitions.
